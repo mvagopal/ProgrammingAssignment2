72b6b2db5442dff3815c59caf7db5e3a31b023bc
usha chandrika <usha chandrika>
2016-04-15 00:01:58 -0400

    made changes to chacematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The function makeCacheMatrix will create a 2X2 by matrix and put it into cache and then create an
+## inverse of that maytrix and cache that too. The inverse is created and cached  when a random number 
+## is greater than 2 and is set to NULL when the random number  is less that 2. 
+## This is to simulate circumstance of cache being available or not.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  givenmatrix <<- function(x) {matrix(c(1,2,3,2), nrow = 2, ncol = 2)
+    
+  }
+  
+  randomnum <- rnorm(5,mean = 2, sd = 1)
+  
+  if( randomnum[1] > 2) {
+    
+    inverseofgivenmatrix <<- solve(givenmatrix())
+    
+  }else {
+    inverseofgivenmatrix <<- NULL
+    
+  }
+  
 }
 
 
-## Write a short comment describing this function
+## cachesolve will calculate inverse of givenmatrix and before it caculates the inverse it checks to see
+## if the inverse is in the cache or not.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  
+  
+  if (is.null(inverseofgivenmatrix)) {
+    inverseofgivenmatrix <<- solve(x)
+    print("computed the inverse")
+  }  else {
+    print("used cache")
+  } 
+  
+  
 }
